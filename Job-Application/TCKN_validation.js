f8a4839138898d3f897dc3a1052ea70c50098675
Hayrettin Yazıcı <hayrettin.yzc.46@gmail.com>
2026-08-09 18:14:16 +0300

    Create TCKN_validation.js

---
+++ Job-Application/TCKN_validation.js
@@ -0,0 +1,35 @@
+function isValidTCKN(tcInput){
+    let digits = tcInput.split("");
+    if(digits.length !== 11){
+        return false;
+    }
+    let numbers = [];
+    for(let i = 0; i < digits.length; i++){
+        digits[i];
+        let value = Number(digits[i]); 
+        if(Number.isNaN(value)){
+            return false;
+        }else{
+            numbers.push(value);
+        }
+    } 
+    if(numbers[0] === 0){
+        return false;
+    }
+    let sumOdd = numbers[0] + numbers[2] + numbers[4] + numbers[6] + numbers[8];
+    let sumEven = numbers[1] + numbers[3] + numbers[5] + numbers[7];
+    let digit10 = ((7 * sumOdd ) - sumEven) % 10;
+    if(digit10 !== numbers[9]){
+        return false;
+    }
+    let sumFirst10 = 0;
+    for (let i = 0; i<10; i++){
+        sumFirst10 = sumFirst10 + numbers[i];
+    }
+    let digit11 = sumFirst10 % 10;
+    if(digit11 !== numbers[10]){
+        return false;
+    }
+
+    return true;
+     } 
